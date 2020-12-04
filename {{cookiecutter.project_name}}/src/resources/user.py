11666fd56b2f4a2b27db8695139417f370feb93c
Serhii Siuziev <snake_wizard@protonmail.com>
2020-12-04 13:59:00 +0200

    Update users resource, small pep8 fixes

--- {{cookiecutter.project_name}}/src/resources/user.py
+++ {{cookiecutter.project_name}}/src/resources/user.py
@@ -44,28 +44,35 @@ class PasswordChange(Resource):
     def put(self):
         request_json = request.get_json(silent=True)
         username = get_jwt_identity()
+
         if not request_json:
-            return {"message": "Body should not be empty"}, 400
+            err_msg = "Body should not be empty"
+            return {"message": err_msg}, 400
+
         old_password: str = request_json.get('old_password')
         new_password: str = request_json.get('new_password')
         confirm_password: str = request_json.get('confirmation_password')
+
         if not old_password or not new_password or not confirm_password:
-            return {
-                       "message": "Something missing in body"
-                   }, 400
+            err_msg = "Please check password fields."
+            return {"message": err_msg}, 400
+
         current_user = UserRepository.get(username)
-        if not UserRepository.verify_hash(old_password, current_user.get("password")):
-            return {
-                "message": "Bad old password"
-            }, 400
+        current_password = current_user.get("password")
+
+        if not current_password:
+            err_msg = "Can't retrieve previous password."
+            return {"message": err_msg}, 400
+        if not UserRepository.verify_hash(old_password, current_password):
+            err_msg = "Wrong old password value."
+            return {"message": err_msg}, 400
         if new_password != confirm_password:
-            return {
-                "message": "New password doesn't match with confirmation password."
-            }, 400
+            err_msg = "New password doesn't match with confirmation password."
+            return {"message": err_msg}, 400
         if not new_password:
-            return {
-                       "message": "New password is empty. Update didn't pass"
-                   }, 400
+            err_msg = "New password is empty. Update didn't pass"
+            return {"message": err_msg}, 400
+
         user = UserRepository.update_password(username, new_password)
         return user, 200
 
@@ -110,24 +117,27 @@ class UserLogin(Resource):
         request_json = request.get_json(silent=True)
         username: str = request_json.get("username")
         password: str = request_json.get("password")
+
         # lookup by username
-        if not username:
-            return {"message": "Bad username"}, 400
-        if not password:
-            return {"message": "Bad password"}, 400
+        if not username or not password:
+            err_msg = "Please check the credentials."
+            return {"message": err_msg}, 400
         if UserRepository.get(username):
             current_user = UserRepository.get(username)
         else:
-            return {"message": "User {} doesn't exist".format(username)}, 404
+            err_msg = f"User {username} doesn't exist"
+            return {"message": err_msg}, 404
 
         if UserRepository.verify_hash(password, current_user.get("password")):
             access_token = create_access_token(identity=username)
             refresh_token = create_refresh_token(identity=username)
-            return {
-                "message": "Logged in as {}".format(current_user.get("username")),
+            msg = "Logged in as {}".format(current_user.get("username"))
+            response = {
+                "message": msg,
                 "access_token": access_token,
                 "refresh_token": refresh_token,
-            }, 200
+            }
+            return response, 200
         else:
             return {"message": "Wrong password"}, 401
 
@@ -139,9 +149,11 @@ class UserLogoutAccess(Resource):
         try:
             revoked_token = RevokedToken(jti=jti, username=get_jwt_identity())
             revoked_token.add()
-            return {"message": "Access token has been revoked"}, 200
+            err_msg = "Access token has been revoked"
+            return {"message": err_msg}, 200
         except IntegrityError:
-            return {"message": "Something went wrong while revoking token"}, 500
+            err_msg = "Something went wrong while revoking token"
+            return {"message": err_msg}, 500
 
 
 class UserLogoutRefresh(Resource):
@@ -151,9 +163,11 @@ class UserLogoutRefresh(Resource):
         try:
             revoked_token = RevokedToken(jti=jti, username=get_jwt_identity())
             revoked_token.add()
-            return {"message": "Refresh token has been revoked"}, 200
+            msg = "Refresh token has been revoked"
+            return {"message": msg}, 200
         except IntegrityError:
-            return {"message": "Something went wrong while revoking token"}, 500
+            err_msg = "Something went wrong while revoking token"
+            return {"message": err_msg}, 500
 
 
 class TokenRefresh(Resource):
