90d4fcfe8219bc4015f4730bd4ece6ee3cc4dba5
Rostyslav Shymchak <rostyslav.shymchak@jetsoftpro.com>
2020-12-12 23:26:03 +0200

    Upgrading ec2-deploy script #20

--- {{cookiecutter.project_name}}/scripts/ec2_deploy.py
+++ {{cookiecutter.project_name}}/scripts/ec2_deploy.py
@@ -4,20 +4,18 @@ import os
 
 def generate_pem(ec2_resource, key_file_name):
     with open(f'{key_file_name}.pem', 'w') as outfile:
-        keys = ec2_resource.create_key_pair(KeyName=key_file_name)
+        keys = ec2_resource.create_key_pair(KeyName=key_file_name, DryRun=False)
         KeyPairOut = str(keys.key_material)
-        # print(KeyPairOut)
         outfile.write(KeyPairOut)
     os.system(f"chmod 400 {key_file_name}.pem")
     return keys
 
 
-
-
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource("ec2")
-pem = generate_pem(ec2_resource, "test2")
+#pem = generate_pem(ec2_resource, "boto_key3")
 user_data = '''#!/bin/bash
+yum -y update
 yum install -y python3
 yum install -y git
 amazon-linux-extras install docker
@@ -28,6 +26,13 @@ curl -L https://github.com/docker/compose/releases/latest/download/docker-compos
 chmod +x /usr/local/bin/docker-compose
 pip3 install cookiecutter
 pip3 install fabric3 
+cd home
+mkdir ec2-user
+cd ec2-user
+cookiecutter https://github.com/shyshach/flask-starter-cookiecutter.git --no-input
+cd test_project
+fab start:staging
+fab init_db:staging
 '''
 instances = ec2_resource.create_instances(
     DryRun=False,
@@ -38,5 +43,16 @@ instances = ec2_resource.create_instances(
     KeyName='boto_key',
     UserData=user_data
 )
-# print(instances)
-# print(pem)
+print("Instance is being created. Just wait a second")
+instances[0].wait_until_running()
+ip = instances[0].private_ip_address
+public_ip = instances[0].public_ip_address
+message = f'''
+Use this ip to ssh into your instance {ip}
+Example ssh command: ssh -i "boto_key.pem" ec2-user@{ip}
+Documentation availble at http://{public_ip}:80/api/docs
+You may have to wait for app to start a minute.
+'''
+with open("server_info.txt", "w+") as f:
+    f.write(message)
+print(message)
